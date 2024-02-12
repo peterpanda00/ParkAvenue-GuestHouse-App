d4a5a50a7f847b385670e0798e5e99adb14bef07
peterpanda00 <jilliane_elloso@dlsu.edu.ph>
2024-02-12 15:31:55 +0800

    + Added Navigation Bar

---
+++ guesthouse_app/frontend/src/routes/Management.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Management() {
+  return (
+    <div className="management">
+      <h1>Management</h1>
+    </div>
+  );
+}
+
+export default Management;
---
+++ guesthouse_app/frontend/src/routes/Restaurant.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Restaurant() {
+  return (
+    <div className="restaurant">
+      <h1>Restaurant</h1>
+    </div>
+  );
+}
+
+export default Restaurant;
