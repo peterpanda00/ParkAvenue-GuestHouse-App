e0e5b98066bc7996b1267903a45e1be1951851a6
Johanna Aperin <114896049+joeMomma07@users.noreply.github.com>
2024-03-11 02:41:41 +0800

    Started Front End for Employee Management

--- guesthouse_app/frontend/src/components/Employees.js
+++ guesthouse_app/frontend/src/components/Employees.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
-import { Card, Table, Button } from "react-bootstrap";
+import { Card, Table, Button, FormControl, Col } from "react-bootstrap";
+import { FaSearch } from "react-icons/fa";
 import supabase from "../config/supabaseClient";
 
 const Employees = () => {
   const [selectedReport, setSelectedReport] = useState("Revenue Report");
+  const [searchQuery, setSearchQuery] = useState("");
 
   const handleReportSelection = (reportType) => {
     setSelectedReport(reportType);
@@ -11,120 +13,121 @@ const Employees = () => {
 
   return (
     <div style={{ width: "100%", padding: "20px" }}>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          marginBottom: "20px",
-        }}
-      >
+      <Col lg="4">
+        <form>
+          <div
+            className="mb-2 mt-3 input-group"
+            style={{
+              maxWidth: "100%",
+              borderRadius: "10px",
+              overflow: "hidden"
+            }}
+          >
+            <input type="search" className="form-control" placeholder="Search"/>
+            <button className="btn me-auto" style={{color: "white", backgroundColor: "#665651"}}>
+              <div style={{color: 'white'}}>
+                {React.createElement(FaSearch, { size: 20 })}
+              </div>
+            </button>
+          </div>
+        </form>
+      </Col>
+
+      <div style={{ display: "flex", justifyContent: "space-between", marginBottom: "20px" }}>
         <Button
-          variant={selectedReport === "Revenue Report" ? "dark" : "light"}
+          variant={selectedReport === "Employee 1" ? "dark" : "light"}
           style={{
             flex: 1,
-            marginRight: "10px",
-            background:
-              selectedReport === "Revenue Report" ? "#665651" : "#D0C8B6",
-            color: selectedReport === "Revenue Report" ? "white" : "#665651",
+            marginRight: "5px",
+            background: selectedReport === "Employee 1" ? "#665651" : "#D0C8B6",
+            color: selectedReport === "Employee 1" ? "white" : "#665651",
           }}
-          onClick={() => handleReportSelection("Revenue Report")}
+          onClick={() => handleReportSelection("Employee 1")}
         >
-          Revenue Report
+          Employee 1
         </Button>
         <Button
-          variant={selectedReport === "Guest Trend Report" ? "dark" : "light"}
+          variant={selectedReport === "Employee 2" ? "dark" : "light"}
           style={{
             flex: 1,
-            marginRight: "10px",
+            marginRight: "5px",
             background:
-              selectedReport === "Guest Trend Report" ? "#665651" : "#D0C8B6",
+              selectedReport === "Employee 2" ? "#665651" : "#D0C8B6",
             color:
-              selectedReport === "Guest Trend Report" ? "white" : "#665651",
+              selectedReport === "Employee 2" ? "white" : "#665651",
           }}
-          onClick={() => handleReportSelection("Guest Trend Report")}
+          onClick={() => handleReportSelection("Employee 2")}
         >
-          Guest Trend Report
+          Employee 2
         </Button>
         <Button
-          variant={selectedReport === "Occupancy Analysis" ? "dark" : "light"}
+          variant={selectedReport === "Employee 3" ? "dark" : "light"}
+          style={{
+            flex: 1,
+            marginRight: "5px",
+            background:
+              selectedReport === "Employee 3" ? "#665651" : "#D0C8B6",
+            color:
+              selectedReport === "Employee 3" ? "white" : "#665651",
+          }}
+          onClick={() => handleReportSelection("Employee 3")}
+        >
+          Employee 3
+        </Button>
+        <Button
+          variant={selectedReport === "Employee 4" ? "dark" : "light"}
           style={{
             flex: 1,
             background:
-              selectedReport === "Occupancy Analysis" ? "#665651" : "#D0C8B6",
+              selectedReport === "Employee 4" ? "#665651" : "#D0C8B6",
             color:
-              selectedReport === "Occupancy Analysis" ? "white" : "#665651",
+              selectedReport === "Employee 4" ? "white" : "#665651",
           }}
-          onClick={() => handleReportSelection("Occupancy Analysis")}
+          onClick={() => handleReportSelection("Employee 4")}
         >
-          Occupancy Analysis
+          +
         </Button>
       </div>
 
-      {selectedReport === "Revenue Report" && (
-        <Card
-          style={{
-            borderRadius: "20px",
-            marginTop: "20px",
-            background: "white",
-            color: "black",
-          }}
-        >
+      {selectedReport === "Employee 1" && (
+        <Card style={{ borderRadius: "20px", marginTop: "20px", background: "white", color: "black", width: "50%", marginRight: "20px" }}>
+        <Table striped bordered hover style={{ borderRadius: "10px" }}>
+          <tbody>
+            <tr>
+              <th style={{ backgroundColor: "#D0C8B6", color: "white", borderTopLeftRadius: "10px" }}>Name</th>
+              <td>John Doe</td>
+            </tr>
+            <tr>
+              <th style={{ backgroundColor: "#D0C8B6", color: "white" }}>Address</th>
+              <td>123 Main St, City</td>
+            </tr>
+            <tr>
+              <th style={{ backgroundColor: "#D0C8B6", color: "white" }}>Email</th>
+              <td>john@example.com</td>
+            </tr>
+            <tr>
+              <th style={{ backgroundColor: "#D0C8B6", color: "white", borderBottomLeftRadius: "10px" }}>Cellphone Number</th>
+              <td>123-456-7890</td>
+            </tr>
+          </tbody>
+        </Table>
+      </Card>
+      )}
+
+
+
+      {selectedReport === "Employee 2" && (
+        <Card style={{ borderRadius: "20px", marginTop: "20px", background: "white", color: "black" }}>
           <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>Booking #</th>
-                <th>Client Name</th>
-                <th>Room Charges</th>
-                <th>Additional Services</th>
-                <th>Other Income Streams</th>
-                <th>Total Revenue</th>
-              </tr>
-            </thead>
-            <tbody></tbody>
+            {/* Table content for Guest Trend Report */}
           </Table>
         </Card>
       )}
 
-      {selectedReport === "Guest Trend Report" && (
-        <Card
-          style={{
-            borderRadius: "20px",
-            marginTop: "20px",
-            background: "white",
-            color: "black",
-          }}
-        >
+      {selectedReport === "Employee 3" && (
+        <Card style={{ borderRadius: "20px", marginTop: "20px", background: "white", color: "black" }}>
           <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>Room Type</th>
-                <th>Average Length of Stay</th>
-                <th>Common Booking Channel</th>
-                <th>Percentage of Repeat Guests</th>
-              </tr>
-            </thead>
-            <tbody>{/* Render guest trend report data here */}</tbody>
-          </Table>
-        </Card>
-      )}
-
-      {selectedReport === "Occupancy Analysis" && (
-        <Card
-          style={{
-            borderRadius: "20px",
-            marginTop: "20px",
-            background: "white",
-            color: "black",
-          }}
-        >
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>Date</th>
-                <th>Occupancy Rate</th>
-              </tr>
-            </thead>
-            <tbody>{/* Render occupancy analysis data here */}</tbody>
+            {/* Table content for Occupancy Analysis */}
           </Table>
         </Card>
       )}
