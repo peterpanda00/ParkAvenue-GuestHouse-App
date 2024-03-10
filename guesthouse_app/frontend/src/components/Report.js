f92ba0a23f7e63d79c49b839601c50a51f4484bb
jilianatan <136918826+jilianatan@users.noreply.github.com>
2024-03-10 21:29:16 +0800

    changed color palette

--- guesthouse_app/frontend/src/components/Report.js
+++ guesthouse_app/frontend/src/components/Report.js
@@ -3,8 +3,7 @@ import { Card, Table, Button } from 'react-bootstrap';
 import supabase from "../config/supabaseClient";
 
 const Report = () => {
-    const [selectedReport, setSelectedReport] = useState('Revenue Report'); // Set initial state to 'Revenue Report'
-    const [bookingList, setBookingList] = useState([]);
+    const [selectedReport, setSelectedReport] = useState('Revenue Report');
 
     const handleReportSelection = (reportType) => {
         setSelectedReport(reportType);
@@ -13,9 +12,27 @@ const Report = () => {
     return (
         <div style={{ width: '100%', padding: '20px' }}>
             <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
-                <Button variant="first" style={{ flex: 1, marginRight: '10px',background: '#665651', color: 'white' }} onClick={() => handleReportSelection('Revenue Report')}>Revenue Report</Button>
-                <Button variant="second" style={{ flex: 1, marginRight: '10px',background: '#665651', color: 'white' }} onClick={() => handleReportSelection('Guest Trend Report')}>Guest Trend Report</Button>
-                <Button variant="third" style={{ flex: 1 ,background: '#665651', color: 'white'}} onClick={() => handleReportSelection('Occupancy Analysis')}>Occupancy Analysis</Button>
+                <Button
+                    variant={selectedReport === 'Revenue Report' ? "dark" : "light"}
+                    style={{ flex: 1, marginRight: '10px', background: selectedReport === 'Revenue Report' ? '#665651' : '#D0C8B6', color: selectedReport === 'Revenue Report' ? 'white' : '#665651' }}
+                    onClick={() => handleReportSelection('Revenue Report')}
+                >
+                    Revenue Report
+                </Button>
+                <Button
+                    variant={selectedReport === 'Guest Trend Report' ? "dark" : "light"}
+                    style={{ flex: 1, marginRight: '10px', background: selectedReport === 'Guest Trend Report' ? '#665651' : '#D0C8B6', color: selectedReport === 'Guest Trend Report' ? 'white' : '#665651' }}
+                    onClick={() => handleReportSelection('Guest Trend Report')}
+                >
+                    Guest Trend Report
+                </Button>
+                <Button
+                    variant={selectedReport === 'Occupancy Analysis' ? "dark" : "light"}
+                    style={{ flex: 1, background: selectedReport === 'Occupancy Analysis' ? '#665651' : '#D0C8B6', color: selectedReport === 'Occupancy Analysis' ? 'white' : '#665651' }}
+                    onClick={() => handleReportSelection('Occupancy Analysis')}
+                >
+                    Occupancy Analysis
+                </Button>
             </div>
 
             {selectedReport === 'Revenue Report' && (
