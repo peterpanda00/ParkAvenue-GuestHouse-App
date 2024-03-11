e813e029c0f060b3b34c03433c4ff790df76e9f4
peterpanda00 <jilliane_elloso@dlsu.edu.ph>
2024-03-11 22:35:14 +0800

    + Added Modal

--- guesthouse_app/frontend/src/components/RestaurantFoodList.js
+++ guesthouse_app/frontend/src/components/RestaurantFoodList.js
@@ -1,12 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState, useEffect } from 'react';
 import { FaFilter, FaSort, FaSearch, FaSave, FaEye, FaEyeSlash, FaTrash } from 'react-icons/fa';
-import { Row, Col, Form, CardBody, Card, Table, InputGroup } from 'react-bootstrap';
+import { Row, Col, Form, CardBody, Card, Table, InputGroup,Modal } from 'react-bootstrap';
 import BookingForm from "./BookingForm"
 import '../index.css';
 import supabase from '../config/supabaseClient';
 
-const RestaurantFoodList = ({ onOfferSubmission }) => {
+const RestaurantFoodList = () => {
     const [hasItems, setHasItems] = useState(false);
     const [isFullView, setIsFullView] = useState(true);
     const [validated, setValidated] = useState(false);
@@ -16,6 +16,8 @@ const RestaurantFoodList = ({ onOfferSubmission }) => {
     const [filterValue, setFilterValue] = useState("");
     const [searchValue, setSearchValue] = useState('');
     const [loading, setLoading] = useState(true); 
+    const [showModal, setShowModal] = useState(false);
+    const [modalMessage, setModalMessage] = useState('');
   
 
 
@@ -54,15 +56,13 @@ const RestaurantFoodList = ({ onOfferSubmission }) => {
       }, []);
 
     const handleSubmit = (event) => {
-        const form = event.currentTarget;
-        if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-        } else {
-            onOfferSubmission()
-        }
-
-        setValidated(true);
+        event.preventDefault();
+        setModalMessage('Ordered Successfully');
+        setShowModal(true);
+        setTimeout(() => {
+            window.location.reload();
+          }, 5000);
+         
     };
 
     const filteredFoodList = foodList
@@ -107,7 +107,7 @@ const RestaurantFoodList = ({ onOfferSubmission }) => {
         const newItem = {
             ...room,
             quantity: 1,
-            discPrice: room.price // Set discPrice to be the same as price initially
+            discPrice: room.ItemPrice // Set discPrice to be the same as price initially
         };
         // Add the new item to the itemList
         setItemList(prevItemList => [...prevItemList, newItem]);
@@ -121,7 +121,7 @@ const RestaurantFoodList = ({ onOfferSubmission }) => {
 
         // Calculate subtotal and total
         itemList.forEach(item => {
-            subtotal += parseFloat(item.price) * item.quantity;
+            subtotal += parseFloat(item.ItemPrice) * item.quantity;
             total += parseFloat(item.discPrice) * item.quantity;
         });
 
@@ -136,7 +136,11 @@ const RestaurantFoodList = ({ onOfferSubmission }) => {
         });
     };
 
-
+    const handleCloseModal = () => {
+        setShowModal(false);
+        // Refresh the page after closing the modal
+        window.location.reload();
+      };
 
     // Calculate totals when itemList changes
     useEffect(() => {
@@ -240,9 +244,9 @@ const RestaurantFoodList = ({ onOfferSubmission }) => {
                                                                             value={item.quantity} onChange={(e) => handleItemListChange(e, index, 'quantity')} />
                                                                     </Form.Group>
                                                                 </td>
-                                                                <td style={{ color: '#665651' }}>{item.name}</td>
+                                                                <td style={{ color: '#665651' }}>{item.ItemName}</td>
                                                                 <td style={{ color: '#665651' }}>
-                                                                    ₱ {formatNumber(item.price)}
+                                                                    ₱ {formatNumber(item.ItemPrice)}
                                                                 </td>
                                                                 <td style={{ color: '#665651' }}>
                                                                     {React.createElement(FaTrash, {
@@ -301,59 +305,24 @@ const RestaurantFoodList = ({ onOfferSubmission }) => {
 
                             {/*Offer List*/}
                             <Col lg="7" >
-                                <Row>
-                                    {/*Filtering Mechanism*/}
-                                    <Col lg="6">
-                                        <div className="mb-2 mt-3 input-group" style={{
-                                            maxWidth: "100%", display: "flex",
-                                            backgroundColor: "#665651", borderRadius: "10px",
-                                            overflow: "hidden"
-                                        }}>
-                                            <div style={{ backgroundColor: "#665651", width: "30px", height: "100%" }}>
-                                                <div style={{ padding: "5px", color: 'white' }}>
-                                                    {React.createElement(FaFilter, { size: 20 })}
-                                                </div>
-                                            </div>
-                                            <select className="form-select">
-                                                <option value="">Breakfast</option>
-                                                <option value="0">Lunch & Dinner</option>
-                                                <option value="1">Beverages</option>
-                                            </select>
-                                        </div>
-                                    </Col>
-                                </Row>
+                            
+                               
 
-                                <Row>
-                                    {/*Search Bar*/}
-                                    <Col lg="6">
-                                        <div className="mb-2 mt-3 input-group" style={{
-                                            maxWidth: "100%", display: "flex",
-                                            backgroundColor: "#665651", borderRadius: "10px",
-                                            overflow: "hidden"
-                                        }}>
-                                            <div style={{ backgroundColor: "#665651", width: "30px", height: "100%" }}>
-                                                <div style={{ padding: "5px", color: 'white' }}>
-                                                    {React.createElement(FaSearch, { size: 20 })}
-                                                </div>
-                                            </div>
-                                            <input type="search" className="form-control" placeholder="Search" />
-                                        </div>
-                                    </Col>
-                                </Row>
 
                                 <div style={{ maxHeight: '78vh', overflowY: 'auto', overflowX: 'hidden' }}>
                                     <Row>
-                                        {filteredFoodList.map((room, index) => (
-                                            <Col className="mt-3" lg="6" key={index}>
-                                                <Card style={{ position: 'relative', height: '250px', width: '200px', cursor: 'pointer', padding: '10px', background: '#665651', color: 'white' }} onClick={() => handleAddToItemList(room)}>
-                                                    <div style={{ position: 'relative', width: '100%', height: '100%', zIndex: 1 }}>
-                                                        <Card.Title style={{ textAlign: 'center', marginBottom: '10px', position: 'relative', zIndex: 2 }}>{room.name}</Card.Title>
-                                                        <Card.Text style={{ textAlign: 'center', position: 'relative', zIndex: 2 }}>{room.type}</Card.Text>
-                                                    </div>
-                                                    <img src={room.imageUrl} style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'cover', zIndex: 0 }} alt={room.name} />
-                                                    <div style={{ backgroundColor: '#665651', color: 'white', textAlign: 'center', position: 'absolute', bottom: '-30px', left: '50%', transform: 'translateX(-50%)', width: '80%', padding: '5px', borderRadius: '5px' }}>{room.name}</div>
-                                                </Card>
-                                            </Col>
+                                                {filteredFoodList.map((food, index) => (
+                                            <Col className="mt-3" key={index} style={{ marginBottom: '20px' }}>
+                                            <Card style={{ position: 'relative', height: '200px', width: '200px', cursor: 'pointer', padding: '10px', background: '#665651', color: 'white' }} onClick={() => handleAddToItemList(food)}>
+                                                <div style={{ position: 'relative', width: '100%', height: '100%', zIndex: 1 }}>
+                                                    <Card.Title style={{ textAlign: 'center', marginBottom: '10px', position: 'relative', zIndex: 2 }}></Card.Title>
+                                                    <Card.Text style={{ textAlign: 'center', position: 'relative', zIndex: 2 }}></Card.Text>
+                                                </div>
+                                                <img src={food.imgURL} style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'cover', zIndex: 0 }} alt={food.ItemName} />
+                                                <div style={{ backgroundColor: '#665651', color: 'white', marginBottom: '110px', textAlign: 'center', position: 'absolute', bottom: '-100px', left: '50%', transform: 'translateX(-50%)', width: '80%', padding: '5px', borderRadius: '5px',fontSize:'15px' }}>{food.ItemName}</div>
+                                            </Card>
+                                        </Col>
+                                        
                                         ))}
                                     </Row>
                                 </div>
@@ -361,6 +330,15 @@ const RestaurantFoodList = ({ onOfferSubmission }) => {
                         </>
                     )}
             </Row>
+
+            <Modal show={showModal} onHide={handleCloseModal}>
+            <Modal.Header closeButton>
+                <Modal.Title>Restaurant Order</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <p>{modalMessage}</p>
+            </Modal.Body>
+            </Modal>
         </>
     );
 };
